e5b20b57573c993cdc90db767856f4ff96c766b3
Robin Y Bobbitt <rbobbitt@redhat.com>
2021-08-04 13:34:28 -0400

    Add clowdapp config support

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	chi "github.com/go-chi/chi/v5"
 	middleware "github.com/go-chi/chi/v5/middleware"
 	redoc "github.com/go-openapi/runtime/middleware"
+
+	"github.com/identitatem/idp-configs-api/config"
 )
 
 func setupDocsMiddleware(handler http.Handler) http.Handler {
@@ -18,28 +20,37 @@ func setupDocsMiddleware(handler http.Handler) http.Handler {
 
 // Handler function that responds with Hello World
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello world")
+	fmt.Fprint(w, "Hello world")
+}
+
+func initDependencies() {
+	config.Init()
+	// l.InitLogger()
+	// db.InitDB()
 }
 
 func main() {
-    // Initialize router
+	initDependencies()
+	cfg := config.Get()
+
+	// Initialize router
 	r := chi.NewRouter()
-	
+
 	r.Use(
 		middleware.Logger,
 		setupDocsMiddleware,
 	)
 
-    // Register handler functions on server routes
+	// Register handler functions on server routes
 	r.Get("/api/hello-world-service/v0/ping", helloWorld)
 
 	// OpenAPI Spec
 	r.Get("/api/hello-world-service/v0/openapi.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./cmd/spec/openapi.json")
+		http.ServeFile(w, r, cfg.OpenAPIFilePath)
 	})
 
-    fmt.Println("Listening on localhost:8080")
+	fmt.Println("Listening on port", cfg.WebPort)
 
-    // Listen and serve using the router
-    http.ListenAndServe(":8080", r)
+	// Listen and serve using the router
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.WebPort), r)
 }
