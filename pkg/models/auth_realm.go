7e1ddcb7de18362c781df958e1c7649f782d7040
Vidya Nambiar <vnambiar@redhat.com>
2021-08-25 18:33:19 -0400

    ftypo fix

--- pkg/models/auth_realm.go
+++ pkg/models/auth_realm.go
@@ -12,7 +12,7 @@ import (
 type BaseModel struct {
 	ID        uint           `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time	     `json:"updated_at_at"`
+	UpdatedAt time.Time	     `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`	
 }
 type AuthRealm struct  {
